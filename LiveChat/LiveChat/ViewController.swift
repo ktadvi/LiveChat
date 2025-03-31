d1ba102de16fe341c708a1bbd68b0e67927bae5a
ktadvi <ktadvi5696@gmail.com>
2025-03-31 00:22:20 -0400

    Add .gitignore for Xcode

---
+++ LiveChat/LiveChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveChat
+//
+//  Created by Khushbu Tadvi on 2025-03-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
